8c47a05b3590022e258d22fb0b8eb2ecd442e04d
MrNoobCobra <bablu@pop-os.localdomain>
2022-02-20 01:20:00 +0530

    Adding state to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './styles/App.scss';
+import react,{ useState } from "react";
+
+//import styles
+import "./styles/App.scss";
+
+//import components
+import React from "react";
+import Song from "./components/Song";
+import Player from "./components/Player";
+
+//import util
+import data from "./util"
 
 function App() {
+
+  const [songs,setSongs]= useState(data());
+  const [currentSong,setCurSong]= useState(songs[0]);
+
   return (
     <div className="App">
-      <h1>Music</h1>
+      <Song currentSong={currentSong} />
+      <Player/>
     </div>
   );
 }
---
+++ src/util.js
@@ -0,0 +1,152 @@
+import { v4 as uuidv4 } from "uuid"
+
+function chillhop()
+{
+    return [
+        {
+            id: uuidv4(),
+            name: "Ending",
+            cover: "https://chillhop.com/wp-content/uploads/2022/01/0ac63dcdc4e16e6bb979f6ea2c9d0a394ee9e386-1024x1024.jpg",
+            artist: "sadtoi, Relyae",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=28969",
+            color: ["#2AB1B7","#67588B"],
+            active: true
+
+        },
+        {
+            id: uuidv4(),
+            name: "Rewind",
+            cover: "https://chillhop.com/wp-content/uploads/2021/12/b3079408b79f0ceed3189d6eb804b448fa0c4068-1024x1024.jpg",
+            artist: "SwuM, afternoon bike ride",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=30134",
+            color: ["#AA6358","#1C303E"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: "Tropical Midnight",
+            cover: "https://chillhop.com/wp-content/uploads/2022/02/7c943e75ba2e1017052e03444366c79445b43195-1024x1024.jpg",
+            artist: "C Y G N",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=31516",
+            color: ["#028444","#536E81"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: "Tuesday",
+            cover: "https://chillhop.com/wp-content/uploads/2020/10/0e5bb63f838ff92eeac142aae944e9f678df13c9-1024x1024.jpg",
+            artist: "Comodo",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=10323",
+            color: ["#E2C3D2","#D14571"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: "perenne",
+            cover: "https://chillhop.com/wp-content/uploads/2021/01/cb5f9d1a28dcbc79a3e0432b8c37aff3406d8271-1024x1024.jpg",
+            artist: " Juan Rios ",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=9216",
+            color: ["#AE9D77","#E6AF58"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: "Curtain Call",
+            cover: "https://chillhop.com/wp-content/uploads/2021/01/cb5f9d1a28dcbc79a3e0432b8c37aff3406d8271-1024x1024.jpg",
+            artist: "Brock Berrigan",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=9240",
+            color: ["#B9A178","#555D4F"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: "Seeds for Tomorrow",
+            cover: "https://chillhop.com/wp-content/uploads/2021/08/af3ce13ad39d38057f00144f8a7c295877ccfec1-1024x1024.jpg",
+            artist: " Smile High, Teddy Roxpin ",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=23336",
+            color: ["#AAD8DC","#CF6165"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: "Odyssey",
+            cover: "https://chillhop.com/wp-content/uploads/2021/08/af3ce13ad39d38057f00144f8a7c295877ccfec1-1024x1024.jpg",
+            artist: " Smile High, Teddy Roxpin ",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=23337",
+            color: ["#AAD8DC","#CF6165"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: "Inside a Saltwater Room",
+            cover: "https://chillhop.com/wp-content/uploads/2021/09/6a9ef8ac00e168d3308fdf9e336874c03fab829d-1024x1024.jpg",
+            artist: "Delayde",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=23193",
+            color: ["#A947C1","#9BD6BC"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: "Aqueduct",
+            cover: "https://chillhop.com/wp-content/uploads/2020/09/88e7eb711f8c71d87fc102e97cf91e36f692348d-1024x1024.jpg",
+            artist: "Leavv",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=9921",
+            color: ["#3F796B","#DA9266"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: "Loving Someone You Lost",
+            cover: "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
+            artist: " The Field Tapes, tender spring, Nuum ",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=11247",
+            color: ["#ABC4C8","#C2CC9B"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: "Sugarless",
+            cover: "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
+            artist: "  The Field Tapes, Aviino  ",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=11243",
+            color: ["#ABC4C8","#C2CC9B"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: "Rest Until Dark",
+            cover: "https://chillhop.com/wp-content/uploads/2020/09/c209a7df7b9bc133dfff73ce86ebc3c57c2b73dd-1024x1024.jpg",
+            artist: "Sleepy Fish",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=10015",
+            color: ["#FCE9D4","#399AB5"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: "Lunar Cycles",
+            cover: "https://chillhop.com/wp-content/uploads/2020/09/c209a7df7b9bc133dfff73ce86ebc3c57c2b73dd-1024x1024.jpg",
+            artist: "Sleepy Fish",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=10031",
+            color: ["#FCE9D4","#399AB5"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: "Lagoons",
+            cover: "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+            artist: " Strehlow, Chris Mazuera ",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=8266",
+            color: ["#C7B9C5","#AE4B49"],
+            active: false
+        }
+    ];
+}
+
+export default chillhop;
+            // id: uuidv4(),
+            // name: "",
+            // cover: "",
+            // artist: "",
+            // audio: "",
+            // color: ["",""],
+            // active: false
