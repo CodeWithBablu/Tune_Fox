4b6f3301647dc1d88e48bfee70379b2e22a3e4f9
MrNoobCobra <bablu@pop-os.localdomain>
2022-03-05 15:59:33 +0530

    Added Turkish Drama

--- src/data.js
+++ src/data.js
@@ -3,6 +3,16 @@ import { v4 as uuidv4 } from "uuid"
 function chillhop()
 {
     return [
+
+        {
+            id: uuidv4(),
+            name: " Kendi Yoluma Gidiyorum ",
+            cover: "https://snoidcdnems07.cdnsrv.jio.com/c.saavncdn.com/001/Kendi-Yoluma-Gidiyorum-Turkish-2018-20180409042041-500x500.jpg",
+            artist: " Aydilge ",
+            audio: "https://sdlhivkecdnems03.cdnsrv.jio.com/jiosaavn.cdn.jio.com/001/0dc5343862a262b19e387340e3dfb5fb_160.mp4",
+            color: ["#ff3f89","#acff27"],
+            active: true
+        },
         {
             id: uuidv4(),
             name: "Ending",
@@ -10,8 +20,7 @@ function chillhop()
             artist: "sadtoi, Relyae",
             audio: "https://mp3.chillhop.com/serve.php/?mp3=28969",
             color: ["#2AB1B7","#67588B"],
-            active: true
-
+            active: false
         },
         {
             id: uuidv4(),
@@ -144,10 +153,236 @@ function chillhop()
             name: "Waybackwhen",
             cover: "https://chillhop.com/wp-content/uploads/2021/11/4c9682ee612a3b8ef51de286c49b5489408e9257-1024x1024.jpg",
             artist: " Philanthrope, Parkbench Epiphany ",
-            audio: "https://pwdown.com/113563/Srivalli - Pushpa.mp3",
+            audio: "https://ac.cf.saavncdn.com/118/036bd1e540a00de38fbc02a1b797d97e_160.mp4?Expires=1646463603&Signature=Jk1A~cw4AAr2db9HOTpVinBU2ZGzlYP6comHmhZgMIWjBTXt0NYcuFuBx1qW7ax-smhl9nibjnQ1e78Rtoep8fJQVElsP66zN970sIBZffiMIE49sJjsIy-3NTDx610Y3PIRT1FKBLfJtSR-BauOwIG7Qpc2JiEH-AWVu9Fdgw4d7y-~H56-AlO7ApWqoiTOdIaG7cUwSxRuvsGmWI4tS5lK662LBfNTE9GEPeOwiZFop8L5nGi8MY-AhKjdfUneNS~DXygD4EJeY3bcC7S~focLW3FcJM1w5EvkJ3rjpZWNjjxuNLZmX4B6Xw9O9ojwzbLiIxKUGztlXZCVTqifEA__&Key-Pair-Id=APKAJB334VX63D3WJ5ZQ",
             color: ["#718697","#F0AA96"],
             active: false
+        },
+        {
+            id: uuidv4(),
+            name: "Asi Ve Aşık",
+            cover: "https://chillhop.com/wp-content/uploads/2021/11/4c9682ee612a3b8ef51de286c49b5489408e9257-1024x1024.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sgdccdnems01.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/6d0d56b44afda12fad990cb932c42ec9_160.mp4",
+            color: ["#718697","#F0AA96"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: "Baht Oyunu (Original Soundtrack)",
+            cover: "https://chillhop.com/wp-content/uploads/2021/11/4c9682ee612a3b8ef51de286c49b5489408e9257-1024x1024.jpg",
+            artist: " Aslı Demirer ",
+            audio: "https://sgdccdnems01.cdnsrv.jio.com/jiosaavn.cdn.jio.com/972/d761345c77eda9b8113cee54e3416a4d_160.mp4",
+            color: ["#718697","#F0AA96"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Aşıklama ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer ",
+            audio: "https://sdlhivkcdnems05.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/b69abea758dd411cf10b4a680bdad2d0_160.mp4",
+            color: ["#F31EF4","#020202"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Kötü Kız ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sdlhivkecdnems06.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/b4e8def4dc5258010820a8f6fc010f7e_160.mp4",
+            color: ["#46b9a0","#9b2c2c"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Afili Aşk Ana Tema (Klarinet Vers.) ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sdlhivkecdnems05.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/a0e8e0ef2a656936dacfdfc870b6a6c4_160.mp4",
+            color: ["#f4a91e","#156af3"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Çaarika ( feat FurKan ) ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sgdccdnems01.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/4a4499e20a474732c3797127d7c17e0f_160.mp4",
+            color: ["#f41e77","#27ffac"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Şişşt ( Sabri ) ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sgdccdnems01.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/c69a78d824549a83cc18cb9caf79cf6a_160.mp4",
+            color: ["#2f2c2f","#3cff00"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Ayşe Ile Kerem ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sgdccdnems02.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/46257156868239849e44f907a7d9467e_160.mp4",
+            color: ["#f41e5e","#00ff9d"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Bapbarap ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sdlhivkecdnems04.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/998f9a8b3948b21ef84eb879d1cfb463_160.mp4",
+            color: ["#b4f41e","#5b02ff"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " İnce İşler (feat Muhsin) ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sgdccdnems01.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/7905e1166837bcf51dd4cf28b3904729_160.mp4",
+            color: ["#5c21ff","#2e2a2a"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Dansöz ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sgdccdnems04.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/13de013b68734164cb45e5ddbf9110a6_160.mp4",
+            color: ["#00b7ff","#00ffbf"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Aşıklama 2 ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sdlhivkecdnems06.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/230ecca30a12880063cadccf84f57b19_160.mp4",
+            color: ["#00af1d","#ff0080"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Plaza ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sgdccdnems02.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/9963b135df778126eb8d5c6faf28698d_160.mp4",
+            color: ["#1ef482","#f632ed"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Çarşı Karıştı ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://snoidcdnems03.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/6139fbaa9e0b6ad0b24629c299b4ab7d_160.mp4",
+            color: ["#d4f41e","#85f529"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Alaturka Ska ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sgdccdnems01.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/afd6c570f91242ca3d6a2ed60f57f67e_160.mp4",
+            color: ["#1ef4a2","#020202"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Durumlar ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sgdccdnems02.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/7d46b06fe478568a5587f072c9db76d7_160.mp4",
+            color: ["#a2f41e","#020202"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Gölge Oyunu ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sdlhivkecdnems04.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/ca2791462f6656ea0bd768c245afb45a_160.mp4",
+            color: ["#e72a2a","#7844f3"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Acı Tatlı ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sklktecdnems02.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/c2a89213f61319431748a1eec6b8ebc8_160.mp4",
+            color: ["#20792c","#2becec"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Kara Komik ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sdlhivkecdnems04.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/91c2fc2bf3740103f1d65653af23dc87_160.mp4",
+            color: ["#b01ef4","#b2ec29"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Enine Boyuna ( feat Raza) ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sklktcdnems01.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/d6c1503f7c52053c20c68876404094d6_160.mp4",
+            color: ["#1ef442","#fd166f"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Mavi Tango ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://snoidcdnems03.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/671936f8d5bad89f2cdda32a7c62d4fe_96.mp4",
+            color: ["#32c1fa","#f3a513"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Haylaz Kalpler ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sklktcdnems06.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/bbf91d8178c6938d2928b304daa91ac8_96.mp4",
+            color: ["#1eedf4","#3d58f1"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Alaylı ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sklktecdnems03.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/d4fb93b9219ace1a927f56b03a889956_96.mp4",
+            color: ["#f4e61e","#6ef644"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Alaturka ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://sdlhivkecdnems05.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/615c972cf2416c883034938bcf9d888b_160.mp4",
+            color: ["#a2f41e","#2ffff5"],
+            active: false
+        },
+        {
+            id: uuidv4(),
+            name: " Sevsin Seni Tarantino ",
+            cover: "https://c.saavncdn.com/195/Afili-A-k-Original-Soundtrack-Vol-1-English-2020-20200407171309-500x500.jpg",
+            artist: " Aslı Demirer, Safa Hendem ",
+            audio: "https://snoidcdnems03.cdnsrv.jio.com/jiosaavn.cdn.jio.com/195/549586e7822db1f8bdc35fc5f7ad8876_160.mp4",
+            color: ["#923fff","#ff2727"],
+            active: false
         }
+        
     ];
 }
 
