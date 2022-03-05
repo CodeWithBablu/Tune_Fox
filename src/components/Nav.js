d2324ed58cd4710260e368ed10467e8622283d5b
MrNoobCobra <bablu@pop-os.localdomain>
2022-03-05 20:50:32 +0530

    Added Turkish Drama

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,17 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";  
-import { faMusic } from "@fortawesome/free-solid-svg-icons";
+import { faMusic, faSpin,  faCompactDisc } from "@fortawesome/free-solid-svg-icons";
+
+//import animation
+import { rotateOut as animate } from "react-animations"
+
+//import styled components
+import styled, { keyframes } from 'styled-components'
 
 const Nav=({ libraryStatus, setLibraryStatus })=>{
 
     return(
         <nav className="nav-bar" >
-            <h1>Waves</h1>
+            <FontAwesomeIcon className="fa-spin" icon={faCompactDisc} color="rgb(9, 255, 124)" size="4x" />
             <button onClick={()=> libraryStatus?setLibraryStatus(false):setLibraryStatus(true) }>
                 Library
                 <span></span>
@@ -14,5 +20,12 @@ const Nav=({ libraryStatus, setLibraryStatus })=>{
         </nav>
     );
 }
+
+
+const Animate= styled.div`
+    
+    animation: 10s ${ keyframes `${ animate }` } infinite;
+
+`
 
 export default Nav;
